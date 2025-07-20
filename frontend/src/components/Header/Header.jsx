faf36c8de2071569413390c99ce9fbf4975437df
Md Rayhan ul Fardous <49313477+rayhan-fardous@users.noreply.github.com>
2025-07-20 15:46:47 +0600

    Header section

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed blanditiis ducimus ipsa nemo consectetur, quod nihil doloremque perferendis velit debitis? Error ullam nemo placeat reiciendis vero ea ipsam incidunt distinctio.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header

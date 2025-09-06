f5a42a2d5537af4b0140130de0459ff3a08baf39
Md Rayhan ul Fardous <49313477+rayhan-fardous@users.noreply.github.com>
2025-09-06 19:52:44 +0600

    add to cart

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,7 +3,7 @@ import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
   
   const {food_list} = useContext(StoreContext)
 

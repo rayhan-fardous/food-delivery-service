2f18dcf4776639e6a46cc1ccaf818dc059b6c530
Md Rayhan ul Fardous <49313477+rayhan-fardous@users.noreply.github.com>
2025-09-11 12:29:12 +0600

    remove food item

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addFood } from "../controllers/foodController.js";
+import { addFood, listfood, removeFood } from "../controllers/foodController.js";
 import multer from "multer";
 
 const foodRouter = express.Router();
@@ -17,6 +17,8 @@ const upload = multer({storage:storage})
 
 
 foodRouter.post("/add",upload.single("image"),addFood)
+foodRouter.get("/list",listfood)
+foodRouter.post("/remove",removeFood);
 
 
 export default foodRouter;
